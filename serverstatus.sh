681ea61d6dddda830a62fa8ae5eced31aa4e05d2
limitrinno <limitrinno@gmail.com>
2021-10-26 18:11:35 +0800

    v4

--- serverstatus.sh
+++ serverstatus.sh
@@ -8,7 +8,7 @@ read -p "探针用户名:(默认:S99)" tzuser
 tzuser=${tzuser:-S99}
 read -p "探针密码:(默认:S99)" tzpwd
 tzpwd=${tzpwd:-S99}
-mkdir /tz && wget --no-check-certificate -qO /tz/client-linux.py 'https://docs.2331314.xyz/api/service/client-linux.py' && nohup python3 /tz/client-linux.py SERVER={$tzip} USER={$tzuser} PASSWORD={$tzpwd} >/dev/null 2>&1 &
+mkdir -p /tz && wget --no-check-certificate -qO /tz/client-linux.py 'https://docs.2331314.xyz/api/service/client-linux.py' && nohup python3 /tz/client-linux.py SERVER={$tzip} USER={$tzuser} PASSWORD={$tzpwd} >/dev/null 2>&1 &
 }
 
 
