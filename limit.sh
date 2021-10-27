a39e31e40a7c805986495ab9fa373045b0c620c9
limitrinno <limitrinno@gmail.com>
2021-10-27 17:41:33 +0800

    v4

--- limit.sh
+++ limit.sh
@@ -51,7 +51,7 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io -y
 systemctl enable docker
-systemctl restart docke
+systemctl restart docker
 echo "Docker 安装完成"
 }
 
